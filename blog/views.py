c1363fc0e65259d8cf9728b44bbc26ddf27dcbb3
Naresh <naresm31@in.ibm.com>
2021-05-23 00:17:06 +0530

    First Commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+def all_blogs(request):
+
+    blogs = Blog.objects.all()
+
+    return render(request, 'blog/all_blogs.html', {'Blogs': blogs})
+
+def details(request, blog_id):
+    
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/details.html', {'blog' : blog })
